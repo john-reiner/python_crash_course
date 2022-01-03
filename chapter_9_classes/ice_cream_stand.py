4d15923f4975b1215eb657fe13fce351c6945ea4
john-reiner <john.reiner@datadoghq.com>
2022-01-03 13:24:29 -0500

    IceCreamStand

---
+++ chapter_9_classes/ice_cream_stand.py
@@ -0,0 +1,38 @@
+class Restaurant():
+    
+    def __init__(self, restaurant_name, cusine_type):
+        self.restaurant_name = restaurant_name
+        self.cusine_type = cusine_type
+        self.open = False
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        if self.open:
+            print("Welcome to " + self.restaurant_name.title() + "! Where we sever " + self.cusine_type.title() + " food!")
+        else: 
+            print("At this time "+ self.restaurant_name.title() + " is closed.")
+    def open_restaurant(self):
+        self.open = True
+        print(self.restaurant_name.title() + " Is now open!")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, number):
+        self.number_served += number
+
+
+class IceCreamStand(Restaurant):
+
+    def __init__(self, restaurant_name, cusine_type):
+        super().__init__(restaurant_name, cusine_type)
+        self.flavors = ['chocolate', 'vanilla', 'strawberry']
+
+    def show_flavors(self):
+        for flavor in self.flavors:
+            print(flavor)
+            
+johnnys = IceCreamStand("crazy johnny's ice cream", "desert")
+
+print(johnnys.show_flavors())
+
